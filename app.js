69bea2519f11bfd38aafdb5e029fe33a01710d2b
AT_Nguyen <anh.tu.nguyen@nuclos.de>
2022-02-06 16:52:46 +0100

    add ejs template

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const util = require('./util.js');
 const app = express();
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-	res.send("Hello, it works!");
+	let message = "Hello EJS a embedded javascript template";
+	let lottoZahlen = util.createLottoNumber();
+	res.render('list', {salute: message, lotto: lottoZahlen});
+
 });
 
 
 
-	
 app.listen(3000, function() {
 	console.log("Server started on port 3000");
 });
---
+++ util.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+  shuffle: function(a){
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  },
+  createLottoNumber: function(){
+    let a = [];
+    for(let i = 0; i < 49; i++){
+    	a[i] = i + 1;
+    }
+    let b = this.shuffle(a);
+    numbers = [];
+    for(let j = 0; j < 6; j++){
+      numbers[j] = a[j];
+    }
+    return numbers;
+  }
+}
